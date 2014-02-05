af3ed12de3e540b5a48f8255ce5969dd27acdc16
gfasanel <giuseppe.fasanella@cern.ch>
2014-02-05 21:08:20 +0100

    first_commit

--- AnalysisScripts/jetanalysis/run_systematics.sh
+++ AnalysisScripts/jetanalysis/run_systematics.sh
@@ -2,42 +2,54 @@
 
 export DISPLAY=""
 
-dir="yr3_systematics_v1"
-mkdir $dir
+dir="my_syst_v1"
+#mkdir $dir
 
-tunes="TuneD6T TuneD6TUEOFF TuneP0 TuneP0UEOFF TuneProPT0 TuneProPT0UEOFF TuneProQ20 TuneProQ20UEOFF TuneUEOFF TuneZ2Star"
+## Il tune e' rimandato => questa sistematica teorica sui Tprime
+##tunes="TuneD6T TuneD6TUEOFF TuneP0 TuneP0UEOFF TuneProPT0 TuneProPT0UEOFF TuneProQ20 TuneProQ20UEOFF TuneUEOFF TuneZ2Star"
 ## tunes="TuneP0 TuneProPT0 TuneUEOFF TuneZ2Star TuneD6T TuneProQ20"
 
-for tune in $tunes; do
-    rm jetanalysis/tmp_datafiles_mvavbftag_tunes.dat{,.pevents}
-    ./mk_fitter.py -i jetanalysis/datafiles_mvavbftag_tunes.dat -n 5 -l ${tune} -o ${dir}/mavtag_${tune}/sub &&  \
-     	./submit_fitter.py -q 8nh -d ${dir}/mavtag_${tune}
-    ### rm jetanalysis/tmp_datafiles_cutbasedvbftag_tunes.dat{,.pevents}
-    ### ./mk_fitter.py -i jetanalysis/datafiles_cutbasedvbftag_tunes.dat -n 5 -l ${tune} -o ${dir}/cutbtag_${tune}/sub && \
-    ###	./submit_fitter.py -q 8nh -d ${dir}/cutbtag_${tune}
-done
+##for tune in $tunes; do
+##    rm jetanalysis/tmp_mydatafile_giuseppe_Tprime_store.dat{,.pevents}
+##    ./mk_fitter.py -i jetanalysis/mydatafile_giuseppe_Tprime_store.dat -n 2 -l ${tune} -o ${dir}/Tprime_${tune}/sub &&  \
+##     	./submit_fitter.py -q 8nh -d ${dir}/Tprime_${tune}
+##done
 
 ## dir="yr3_systematics_v1"
 ## mkdir $dir
 
-jecUncs=""
-## jecUncs="jerCentral"
-## jecUncs="nominal jecUp jecDown jerCentral jerUp jerDown"
+jecUncs="nominal jecUp jecDown jerCentral jerUp jerDown"
 
 for syst in $jecUncs; do
-    ./mk_fitter.py -i jetanalysis/datafiles_mvavbftag_jec.dat -n 5 -l ${syst} -o ${dir}/mvatag_${syst}/sub && \
-	./submit_fitter.py -q 8nh -d ${dir}/mvatag_${syst}
-    ./mk_fitter.py -i jetanalysis/datafiles_cutbasedvbftag_jec.dat -n 5 -l ${syst} -o ${dir}/cutbtag_${syst}/sub && \
-	./submit_fitter.py -q 8nh -d ${dir}/cutbtag_${syst}
+    rm jetanalysis/tmp_mydatafile_giuseppe_tth_store_jec_btag.dat{,.pevents}
+    rm -r ${dir}/tthTprimetag_${syst}/sub*
+    ./mk_fitter.py -i jetanalysis/mydatafile_giuseppe_tth_store_jec_btag.dat -n 2 -l ${syst} -o ${dir}/tthTprimetag_${syst}/sub && \
+	./submit_fitter.py -q 1nh -d ${dir}/tthTprimetag_${syst}
 done
 
-btagEff=" noBtagSF"
-#btagEff=" noBtagSF nominalBtagSF shiftBtagEffUp_bc shiftBtagEffDown_bc shiftBtagEffUp_l shiftBtagEffDown_l"
+comment1()
+{
+#JER JEC
+#jecUncs="jerCentral"
+jecUncs="nominal jecUp jecDown jerCentral jerUp jerDown"
 
+for syst in $jecUncs; do
+    rm jetanalysis/tmp_mydatafile_giuseppe_Tprime_store_jec_btag.dat{,.pevents}
+    rm -r ${dir}/Tprimetag_${syst}/sub*
+    ./mk_fitter.py -i jetanalysis/mydatafile_giuseppe_Tprime_store_jec_btag.dat -n 2 -l ${syst} -o ${dir}/Tprimetag_${syst}/sub && \
+	./submit_fitter.py -q 1nh -d ${dir}/Tprimetag_${syst}
+done
+}
+
+comment1 ()
+{
+#BTAG
+btagEff=" noBtagSF nominalBtagSF shiftBtagEffUp_bc shiftBtagEffDown_bc shiftBtagEffUp_l shiftBtagEffDown_l"
 
 for systb in $btagEff; do
-    ./mk_fitter.py -i jetanalysis/datafiles_mva_btag.dat -n 5 -l ${systb} -o ${dir}/mvabtag_${systb}/sub && \                                               
-        ./submit_fitter.py -q 8nh -d ${dir}/btag_${systb}
-    ./mk_fitter.py -i jetanalysis/datafiles_cutbased_btag.dat -n 15 -l ${systb} -o ${dir}/cutbtag_${systb}/sub && \
-	./submit_fitter.py -q 8nh -d ${dir}/btag_${systb}
+    rm jetanalysis/tmp_mydatafile_giuseppe_Tprime_store_jec_btag.dat{,.pevents}
+    rm -r ${dir}/Tprimetag_${systb}/sub*
+    ./mk_fitter.py -i jetanalysis/mydatafile_giuseppe_Tprime_store_jec_btag.dat -n 2 -l ${systb} -o ${dir}/Tprimetag_${systb}/sub && \
+	./submit_fitter.py -q 1nh -d ${dir}/Tprimetag_${systb}
 done
+}
